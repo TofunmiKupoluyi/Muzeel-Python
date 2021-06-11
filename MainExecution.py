44609509688dc17f437da051fe9df161aaa2b185
tofunmikupoluyi <tofunmi@live.com>
2021-06-12 00:28:30 +0400

    Cleaned up arguments

--- MainExecution.py
+++ MainExecution.py
@@ -4,10 +4,10 @@ from browser_interaction_bot.ChromeExecution import ChromeExecution
 from browser_interaction_bot.event_handling.DefaultEventHandler import DefaultEventHandler
 
 
-def execute(db_details: dict, site: str, proxy_url: str) -> None:
+def execute(site: str, db_details: dict, proxy_url: str, output_file_directory: str = None) -> None:
     data_store = DataStore(site, db_details)
     data_store.persist_updated_files()
-    chrome_execution = ChromeExecution(site, DefaultEventHandler(), proxy_url=proxy_url)
+    chrome_execution = ChromeExecution(site, DefaultEventHandler(), proxy_url=proxy_url, output_file_directory=output_file_directory)
     try:
         chrome_execution.execute()
         used_function_id_map = LogParser.parse_logs(chrome_execution.logs, data_store.request_url_content_file_map)
